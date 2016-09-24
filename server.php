d7cd952dd488496bae389583b19b84f184472bde
wangyupeng <wangyupeng129@126.com>
2016-09-24 23:13:35 +0800

    增加测试输出

---
+++ server.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once 'libs/function.php';
+
+while (true) {
+
+    $process_list = get_cache('process_list');
+
+    if (!$process_list) {
+        sleep(1);
+        continue;
+    }
+
+    foreach ($process_list as $id) {
+        exec('php wx_listener.php ' . $id . ' >/dev/null &');
+        _echo('启动进程, 用户ID: '.$id);
+    }
+
+    del_cache('process_list');
+}
